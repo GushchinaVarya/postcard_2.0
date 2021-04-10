e3f7ee5dead00610546b2134c22d6faa52049b2d
Varvara Sologub <ipod_touch@mail.ru>
2021-04-10 23:54:57 +0300

    bday reminder test version 01

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from appearance_funtions import *
 from telegram import InputMediaPhoto, InputFile
 import numpy as np
 import pandas as pd
+import datetime
 
 logger = getLogger(__name__)
 
@@ -714,6 +715,83 @@ def cancel_handler(update: Update, context: CallbackContext) -> int:
     )
     return ConversationHandler.END
 
+
+def alarm(context: CallbackContext):
+    keyboard = [
+        [InlineKeyboardButton(BUTTON2_MAKE, callback_data=CALLBACK_BUTTON2_MAKE)],
+    ]
+    job = context.job
+    context.bot.send_message(
+        job.context,
+        text='Привет! С наступающим днем рождения! Пришлашаем тебя создать вишлист!',
+        reply_markup=InlineKeyboardMarkup(keyboard, one_time_keyboard=True),
+        parse_mode=ParseMode.MARKDOWN
+    )
+
+
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+def set_timer_bday(update: Update, context: CallbackContext) -> None:
+    """Add a job to the queue."""
+    keyboard = [
+        [InlineKeyboardButton(BUTTON2_MAKE, callback_data=CALLBACK_BUTTON2_MAKE)],
+    ]
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the date format DD.MM for the timer in seconds
+        dateofbirth = context.args[0]
+        dayofbirth = int(dateofbirth.split('.')[0])
+        monthofbirth = int(dateofbirth.split('.')[1])
+        yearcurrent = datetime.datetime.today().year
+        if (datetime.date(yearcurrent, monthofbirth, dayofbirth) - datetime.datetime.today().date()).days == 0:
+            update.message.reply_text(
+                text='C днем рождения! Пришлашаем тебя создать вишлист!',
+                reply_markup=InlineKeyboardMarkup(keyboard, one_time_keyboard=True),
+            )
+            due = datetime.datetime(yearcurrent + 1, monthofbirth, dayofbirth, 12)
+        elif (datetime.date(yearcurrent, monthofbirth, dayofbirth) - datetime.datetime.today().date()).days == 1:
+            update.message.reply_text(
+                text='Привет! С наступающим днем рождения! Пришлашаем тебя создать вишлист!',
+                reply_markup=InlineKeyboardMarkup(keyboard, one_time_keyboard=True),
+            )
+            due = datetime.datetime(yearcurrent + 1, monthofbirth, dayofbirth, 12)
+        elif (datetime.date(yearcurrent, monthofbirth, dayofbirth) - datetime.datetime.today().date()).days > 1:
+            due = datetime.datetime(yearcurrent, monthofbirth, dayofbirth, 12)
+            update.message.reply_text('Спасибо! Мы напомним тебе о создании вишлиста за день до твоего дня рождения!\nВернуться в главное меню - /start')
+        else:
+            due = datetime.datetime(yearcurrent+1, monthofbirth, dayofbirth, 12)
+            update.message.reply_text('Спасибо! Мы напомним тебе о создании вишлиста за день до твоего дня рождения!\nВернуться в главное меню - /start')
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(alarm, when=due, context=chat_id, name=str(chat_id))
+
+        user_id_df_bday = pd.read_csv(USER_IDS_FILE_BDAY, index_col=0)
+        if chat_id not in user_id_df_bday.user_id.values:
+            user_id_df_bday = user_id_df_bday.append(pd.DataFrame({'user_id': np.array([chat_id]), 'bday':np.array([dateofbirth])})).reset_index(drop=True)
+            user_id_df_bday.to_csv(USER_IDS_FILE_BDAY)
+            logger.info('added to file of users birthdays chat_id: %s', chat_id)
+        if chat_id in user_id_df_bday.user_id.values:
+            user_id_df_bday.loc[[user_id_df_bday[user_id_df_bday.user_id == chat_id].index[0]], ['bday']] = dateofbirth
+            user_id_df_bday.to_csv(USER_IDS_FILE_BDAY)
+            logger.info('updated file of users birthdays chat_id: %s', chat_id)
+
+        logger.info(f'{update.message.chat.id} successfully set timer')
+        if job_removed:
+            logger.info(f'old {update.message.chat.id} timer was removed')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /mybday <DD.MM>')
+
+
+
 @debug_request
 def about(update: Update, context: CallbackContext):
     context.user_data[WISH_MODE] = 'False'
@@ -815,6 +893,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('about', about))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
     updater.dispatcher.add_handler(MessageHandler(Filters.photo, photo_handler))
+    updater.dispatcher.add_handler(CommandHandler('mybday', set_timer_bday))
 
     updater.start_polling()
     updater.idle()
